23d90ef3ad60c9cddca3ad0f6bd56ef22192ed5c
sukumarsaurav <sukumarsaurav@gmail.com>
2024-12-22 14:48:27 +0530

    style

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ try {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Digital Agency - Your One-Stop Solution</title>
         <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/services.css">
     </head>
     <body>
         <!-- Header -->
