de95f6b21205733070636cfdb197e731aaf7ad40
sukumarsaurav <sukumarsaurav@gmail.com>
2024-12-22 16:46:19 +0530

    stle

--- index.php
+++ index.php
@@ -29,9 +29,8 @@ try {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Digital Agency - Your One-Stop Solution</title>
-        <link rel="stylesheet" href="assets/css/style.css">
         <link rel="stylesheet" href="assets/css/home.css">
-    </head>
+        <link rel="stylesheet" href="assets/css/style.css">
     <body>
         <!-- Header -->
         <header class="header">
